40801162c3db9b6623836efffb84efcae1d75bec
Katherine Abrikian <kabrikian@gmail.com>
2023-02-01 23:04:16 -0500

    unused file cleanup, tested AG Grid, created route for EventAttendance

---
+++ src/routes/EventAttendance.js
@@ -0,0 +1,11 @@
+import "./EventAttendance.css";
+
+const EventAttendance = () => {
+  return (
+    <div>
+      <p>This is the EventAttendance route.</p>
+    </div>
+  );
+};
+
+export default EventAttendance;
